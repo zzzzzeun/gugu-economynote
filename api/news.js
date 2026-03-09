34b5a13b528f86ffc5da2ec4fe7ea3a11f3d3867
zzzzzeun <zzzzzeun@gmail.com>
2026-03-09 17:32:25 +0900

    Update news.js

--- api/news.js
+++ api/news.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   const apiKey = "f923a6c18ee647ef9eee5be98ae618b7";
 
   const response = await fetch(
-    `https://newsapi.org/v2/top-headlines?country=kr&category=business&pageSize=5&apiKey=${apiKey}`
+    `https://newsapi.org/v2/everything?q=경제 OR 금리 OR 환율 OR 증시&language=ko&sortBy=publishedAt&pageSize=5&apiKey=${apiKey}`
   );
 
   const data = await response.json();
